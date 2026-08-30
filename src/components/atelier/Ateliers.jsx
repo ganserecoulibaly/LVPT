c1149adf2942280c453b26a6612d3ef4533a9016
gansere <gansere.coulibaly@gmail.com>
2026-08-31 00:47:34 +0200

    spa

--- src/components/atelier/Ateliers.jsx
+++ src/components/atelier/Ateliers.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
-import { useFavoriLieuxEtPlats } from '../useFavoriLieuxPlatsSpas'
+import { useFavoriLieuxPlatsSpas } from '../useFavoriLieuxPlatsSpas'
 import Sidebar from '../Sidebar'
 import PageHeader from '../PageHeader'
 import EditProfileModal from '../EditProfileModal'
